819e4cb4717062479a268239d87617374ef76db2
Panji Ananda <panjianandalumbantoruan@gmail.com>
2025-07-25 18:48:47 +0700

    Icon BoanAksara

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,12 @@
     <!-- Navigation-->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
         <div class="container px-4 px-lg-5">
-            <a class="navbar-brand" href="#page-top">BoanAksara</a>
+            <a class="navbar-brand d-flex align-items-center gap-2" href="#page-top" style="gap: 8px;">
+    <img src="{{ asset('assets/img/logoboanAksara.png') }}" alt="Logo BoanAksara" style="height: 48px; width: auto;" />
+    
+    <span class="align-middle" style="font-size: 1.25rem;">BoanAksara</span>
+</a>
+
             <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ms-auto my-2 my-lg-0">
